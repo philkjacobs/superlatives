0a16deaa13eef39bc952e67e80a4b8f7704ccd24
Mathew <mathew.antony31@gmail.com>
2017-09-09 16:26:12 -0500

    More changes

--- web/js/main.jsx
+++ web/js/main.jsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class Component extends React.Component {
+class Application extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome to Superlatives</h1>
-        <button onClick={this.hostGameButtonPresed}>Host Game</button>
-        <button onClick={this.joinGameButtonPresed}>Join Game</button>
+        <button onClick={this.hostGameButtonPressed}>Host Game</button>
+        <button onClick={this.joinGameButtonPressed}>Join Game</button>
       </div>
     )
   }
+
+  hostGameButtonPressed(){
+    console.log("Host button pressed");
+  }
+
+  joinGameButtonPressed(){
+    console.log("Join game button pressed");
+  }
+
 }
 
 ReactDOM.render(
-  <Component />,
+  <Application />,
   document.getElementById('root')
 );
