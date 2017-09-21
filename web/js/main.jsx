1b392d7b7f0a388e9f94a3359226678dacf21538
Mathew <mathew.antony31@gmail.com>
2017-09-20 22:44:11 -0500

    Cleaned up main.jsx file

--- web/js/main.jsx
+++ web/js/main.jsx
@@ -3,128 +3,18 @@ import * as ReactDOM from 'react-dom';
 import Notifications, {notify} from 'react-notify-toast';
 import AssignSupers from './components/AssignSupers.jsx';
 import WriteSupers from './components/WriteSupers.jsx';
-
-// Hard-coding player names. These will eventually live-update as new players are added to the database for a given gameID
-var players = ['Mathew', 'Jeremy', 'Ben', 'Philip', 'Julia', 'Brian']
-
-function Player(props){
-	return(
-		<div>
-			{props.name}
-		</div>
-	)
-}
-
-class WaitingRoom extends React.Component {
-  render(){
-    return(
-      <div>
-        <h3>Waiting Room</h3>
-        {this.props.players.map(function(player){
-            return (
-              <Player name={player} />
-            );
-        }.bind(this))}
-      </div>
-    )
-  }
-}
-
-function Player(props){
-  return(
-    <div>
-      {props.name}
-    </div>
-  );
-}
-
-class HostGame extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      gameId: props.gameId,
-      startGame: false
-    }
-
-    this.startGameButtonPressed = this.startGameButtonPressed.bind(this)
-
-  }
-
-  render(){
-    return(
-      <div>
-        <h3>Heres your special game code. Share this with your friends </h3>
-        {this.state.gameId}
-        <WaitingRoom players={players} />
-        <button className="btn-rounded btn-outlined orange-btn" onClick={this.startGameButtonPressed}>
-          Start Game
-        </button>
-        {this.state.startGame ? <WriteSupers /> : null}
-      </div>
-    )
-  }
-
-  fetchGameID(){
-    // Magic code to fetch game ID
-  }
-
-  startGameButtonPressed(){
-  	// Change game state so other devices will start the game too
-  	this.setState({
-  		startGame: true
-  	})
-  }
-}
-
-HostGame.propTypes = {
-  gameId: React.PropTypes.string.isRequired
-};
-
-HostGame.defaultProps = {
-  gameId: "TEST GAME"
-}
-
-class JoinGame extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      goToWaitingRoom: false
-    }
-    this.onSubmit = this.onSubmit.bind(this)
-  }
-  render(){
-    return(
-      <div>
-        <h3>Enter magic code to join game:</h3>
-        <form onSubmit={this.onSubmit}>
-            <label>
-              <input type="text" />
-            </label>
-            <input type="submit" value="Join Game" />
-          </form>
-          {this.state.goToWaitingRoom ? <WaitingRoom players={players} /> : null}
-      </div>
-      // Listen for server to tell us to start game
-    )
-  }
-
-  onSubmit(e){
-    e.preventDefault();
-    // For now, it's always true. Eventually, it'll do a check before going to the waiting room
-    this.setState({
-    	goToWaitingRoom: true
-    });
-  }
-}
+import * as Menu from './components/Menu.jsx';
 
 class Application extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      isHost:false,
       showHostScreen: false,
       showWaitingScreen:false,
-      shouldStartGame:false,
-      statusText:""
+      statusText:"",
+      gameId:"FOOBAR",
+      gameState:"none"
     }
 
     this.hostGameButtonPressed = this.hostGameButtonPressed.bind(this);
@@ -139,30 +29,23 @@ class Application extends React.Component {
         <p>{this.state.statusText}</p>
         <button onClick={this.hostGameButtonPressed} className="btn-rounded btn-outlined orange-btn">Host Game</button>
         <button onClick={this.joinGameButtonPressed} className="btn-rounded btn-outlined green-btn">Join Game</button>
-        {this.state.showHostScreen ? <HostGame /> : <JoinGame />}
+        {this.state.showHostScreen ? <Menu.WaitingRoom players={Menu.players} isHost={this.state.isHost} gameId={this.state.gameId} /> : <Menu.JoinGame gameId={this.state.gameId}/>}
       </div>
     )
   }
 
   hostGameButtonPressed(){
-    console.log("Host button pressed");
-    
     this.setState({
+      isHost: true,
       showHostScreen: true
     });
-
   }
 
   joinGameButtonPressed(){
-    console.log("Join game button pressed");
-
     this.setState({
-      showHostScreen: false
+      showHostScreen: false,
+      isHost: false
     });
-  }
-
-  goToWaitingRoom(){
-    notify.show('Not yet built!');
   }
 
   goToWaitingScreen(status, statusText){
@@ -172,6 +55,21 @@ class Application extends React.Component {
     });
   }
 
+  login(data){
+    // Send player name and gameId (only if joining)
+  }
+
+  changeGameState(state){
+    // Send message to server with new game state
+  }
+
+  writeSuper(data){
+    // Write super to server
+  }
+
+  assignSuper(data){
+    // Writer super object to server
+  }
 }
 
 ReactDOM.render(
