0b81e9de581b991f0c0f526cbac7587ca7cb00f2
Mathew Antony <mathew.antony31@gmail.com>
2017-09-25 20:23:36 -0500

    Break code out into components (#19)
    * Cleaning up main.jsx
    
    * Added more to write supers
    
    * More changes
    
    * Added superlative file
    
    * Cleaned up main.jsx file
    
    * Added more changes
    
    * More changess
    
    * Added more changes
    
    * More changes
    
    * Broken out code into smaller components
    
    * Removed DS Store file

---
+++ web/js/components/Menu.jsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import WriteSupers from './WriteSupers.jsx';
+import Notifications, {notify} from 'react-notify-toast';
+
+// Hard-coding player names. These will eventually live-update as new players are added to the database for a given gameID
+export var players = ['Mathew', 'Jeremy', 'Ben', 'Philip', 'Julia', 'Brian']
+
+export function Player(props){
+  return(
+    <div>
+      {props.name}
+    </div>
+  )
+}
+
+export class WaitingRoom extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      startGame: false
+    }
+    this.startGameButtonPressed = this.startGameButtonPressed.bind(this)
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.startGame ? <WriteSupers submitSuper={this.props.submitSuper} /> : <div>
+        <h3>Heres your special game code: {this.props.gameId}.</h3>
+        <p>Share this with your friends</p>
+          <h3>Waiting Room</h3>
+          {this.props.players.map(function(player){
+              return (
+                <Player name={player} />
+              );
+          }.bind(this))}
+          {this.props.isHost ? <button className="btn-rounded btn-outlined orange-btn" onClick={this.startGameButtonPressed}>
+            Start Game
+          </button> : null}
+        </div>}
+      </div>
+    )
+  }
+
+  startGameButtonPressed(){
+    // Change game state so other devices will start the game too
+    // TODO: This needs to change state at the Application level
+    this.setState({
+      startGame: true
+    })
+  }
+}
+
+WaitingRoom.propTypes = {
+  gameId: React.PropTypes.string.isRequired,
+  isHost: React.PropTypes.Bool
+}
+
+export class JoinGame extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      goToWaitingRoom: false
+    }
+    // this.onSubmit = this.onSubmit.bind(this)
+  }
+  render(){
+    return(
+      <div>
+          {this.state.goToWaitingRoom ? <WaitingRoom players={players} gameId={this.props.gameId}/> : <div>
+          <h3>Enter magic code to join game:</h3>
+            <form onSubmit={this.props.onSubmit(this.props.gameId)}>
+              <label>
+                <input type="text" value={this.props.gameId} onChange={this.props.onChange}/>
+              </label>
+              <input type="submit" value="Join Game" />
+            </form>
+          </div>}
+      </div>
+      // Listen for server to tell us to start game
+    )
+  }
+
+  // onSubmit(e){
+  //   e.preventDefault();
+  //   // For now, it's always true. Eventually, it'll do a check before going to the waiting room
+  //   this.setState({
+  //     goToWaitingRoom: true
+  //   });
+  // }
+}
