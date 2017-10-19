3ba0584b4da7d8270c7d6668c628201633bee457
Philip <philip@dropbox.com>
2017-10-18 20:54:20 -0500

    tests through starting game complete + refactoring game state to an enum and the messaging helpers to be accessible inside tests

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import json
 import logging
 
+from game_state import (
+    broadcast_message,
+    GameStates,
+    send_message,
+)
+
 from setup import GlobalApplication
 
 from tornado.httpserver import HTTPServer
@@ -31,9 +37,8 @@ class TestWebsocketHandler(AsyncTestCase):
 
         assert_valid_login_message(response, ['Phil'])
 
-
     @gen_test
-    def test_create_game_two_player(self):
+    def test_join_game(self):
         host = "Phil"
         player = "Mathew"
 
@@ -46,7 +51,6 @@ class TestWebsocketHandler(AsyncTestCase):
         assert_valid_login_message(response, [host])
 
         game = response['data']['game']
-        logging.error(game)
         player_connection = yield websocket_connect(
             'ws://localhost:{}/ws?name={}&game={}'.format(self.port, player, game)
         )
@@ -59,12 +63,100 @@ class TestWebsocketHandler(AsyncTestCase):
         response = json.loads(response)
         assert_valid_login_message(response, [host, player])
 
-def assert_valid_login_message(response, player_list):
-    assert 'data' in response
-    assert 'msg' in response
-    assert 'error' in response
+    @gen_test
+    def test_change_state_write_host(self):
+        c = yield websocket_connect(
+            'ws://localhost:{}/ws?name={}'.format(self.port, 'Phil')
+        )
 
-    assert response['msg'] == 'login'
-    assert all(key in response['data'] for key in ('game', 'players'))
-    assert len(response['data']['players']) == len(player_list)
-    assert all(player in player_list for player in response['data']['players'])
+        _ = yield c.read_message()  # login message
+
+
+        send_message(
+            c,
+            message="change_state",
+            data={'state': GameStates.WRITE.value},
+        )
+
+        response = yield c.read_message()
+        response = json.loads(response)
+        assert response['data'] is not None, response
+        assert response['error'] is None, response
+
+    @gen_test
+    def test_change_state_write_not_host_error(self):
+        host = "Phil"
+        player = "Mathew"
+
+        host_connection = yield websocket_connect(
+            'ws://localhost:{}/ws?name={}'.format(self.port, host)
+        )
+
+        response = yield host_connection.read_message()  # login message
+        response = json.loads(response)
+        game = response['data']['game']
+        player_connection = yield websocket_connect(
+            'ws://localhost:{}/ws?name={}&game={}'.format(self.port, player, game)
+        )
+
+        _ = yield player_connection.read_message() # login message
+
+
+        send_message(
+            player_connection,
+            message="change_state",
+            data={'state': GameStates.WRITE.value},
+        )
+
+        response = yield player_connection.read_message()
+        response = json.loads(response)
+        assert response['data'] is None, response
+        assert response['error'] is not None, response
+
+    @gen_test
+    def test_change_state_write_all_players_recv_message(self):
+        host = "Phil"
+        player = "Mathew"
+
+        host_connection = yield websocket_connect(
+            'ws://localhost:{}/ws?name={}'.format(self.port, host)
+        )
+
+        response = yield host_connection.read_message()  # login message
+        response = json.loads(response)
+
+        game = response['data']['game']
+        player_connection = yield websocket_connect(
+            'ws://localhost:{}/ws?name={}&game={}'.format(self.port, player, game)
+        )
+
+        _ = yield player_connection.read_message() # login message
+
+
+        send_message(
+            host_connection,
+            message="change_state",
+            data={'state': GameStates.WRITE.value},
+        )
+
+        response = yield host_connection.read_message()
+        response = json.loads(response)
+        assert response['data'] is not None, response
+        assert response['error'] is None, response
+
+        response = yield player_connection.read_message()
+        response = json.loads(response)
+        assert response['data'] is not None, response
+        assert response['error'] is None, response
+
+
+
+def assert_valid_login_message(response, player_list):
+    assert 'data' in response, response
+    assert 'msg' in response, response
+    assert 'error' in response, response
+
+    assert response['msg'] == 'login', response['msg']
+    assert all(key in response['data'] for key in ('game', 'players')), response['data']
+    assert len(response['data']['players']) == len(player_list), '{} {}'.format(len(response['data']['players']), len(player_list))
+    assert all(player in player_list for player in response['data']['players']), response['data']['players']
