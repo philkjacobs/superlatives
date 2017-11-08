5bfc3a983f375c4db60794f85ad8f67afe625c96
Mathew <mathew.antony31@gmail.com>
2017-11-08 10:48:45 -0600

    Updated local IP

--- web/js/main.jsx
+++ web/js/main.jsx
@@ -174,10 +174,10 @@ return(<div className="player">{player}</div>)
 
     switch(type){
       case 'host':
-        socket = new WebSocket(`ws://192.168.1.64:5000/ws?name=${this.state.playerName}`);
+        socket = new WebSocket(`ws://172.21.5.204:5000/ws?name=${this.state.playerName}`);
         break;
       case 'join':
-        socket = new WebSocket(`ws://192.168.1.64:5000/ws?name=${this.state.playerName}&game=${this.state.gameId}`);
+        socket = new WebSocket(`ws://172.21.5.204:5000/ws?name=${this.state.playerName}&game=${this.state.gameId}`);
         break;
       default:
         console.log("Error: Incorrect type. Expected host or join.")
