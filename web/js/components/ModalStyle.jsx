f2860318ec5e930cd66e904aab7dbf78be4b5512
Mathew <mathew.antony31@gmail.com>
2017-12-02 19:35:16 -0600

    Added modal style file

---
+++ web/js/components/ModalStyle.jsx
@@ -0,0 +1,25 @@
+export const MODAL_STYLE = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.91)'
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    left                       : '10px',
+    right                      : '10px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#2F3A3A',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px',
+    color                      : 'white'
+  }
+};
