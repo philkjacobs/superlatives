f6740f21deceb33873c832449bba050e7f7f5777
Mathew <mathew.antony31@gmail.com>
2018-02-07 21:03:36 -0600

    Added copy saying were waiting for host to start game in waiting room

--- web/js/components/Menu.jsx
+++ web/js/components/Menu.jsx
@@ -89,6 +89,7 @@ export class WaitingRoom extends React.Component {
                                                   isOpen={this.state.showModal}
                                                   toggleModal={function(){this.toggleModal()}.bind(this)}/> : null}
           <h2 className="code">GAME CODE: {this.props.gameId}</h2>
+          {this.props.isHost ? null : <div className="waiting-for-host">Waiting for Host to start game</div>}
           <h1>Waiting Room</h1>
           <div className="player-list">
             {this.props.players.map(function(player){
